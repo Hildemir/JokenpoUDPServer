a345a16f4a8eac7e8822a46fe5df8851a34d443e
Hildemir <hildemir.regis@hotmail.com>
2020-06-25 21:47:03 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Jokenpo Game UDP Server
+
+### 📝 About the project
+- This project was was the result of playing with Socket Connections.
+- This version is a simple test of a client who plays with a server without graphical interface.
+---
+### 👨‍💻 Technologies
+- [Java](https://www.oracle.com/technetwork/pt/java/javase/downloads/index.html)
+---
+### ▶ How to run this project
+- Download this project
+
+```bash 
+# clone this repository (client) to your machine
+$ git clone https://github.com/Hildemir/JokenpoUDPClient
+# Then do the same with the server 
+$ git clone https://github.com/Hildemir/JokenpoUDPServer
+```
+
+1. [Download Java](https://www.oracle.com/technetwork/pt/java/javase/downloads/index.html)
+    - [Setup enviroment](https://www.tutorialspoint.com/java/java_environment_setup.htm)
+2. Open each project in different windows.
+3. Run first the server and then the client.
+4. Time to play!
+---
+Developed by Hildemir Regis 😜
---
+++ src/UDPServer.java
@@ -0,0 +1,170 @@
+
+    /*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+     */
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+    public class UDPServer {
+
+public static final String ANSI_RED = "\u001B[31m";
+        public static final String ANSI_GREEN = "\u001B[32m";
+        public static final String ANSI_RESET = "\u001B[0m";
+        public static final String ANSI_YELLOW = "\u001B[33m";
+        public static final String ANSI_BLUE = "\u001B[34m";
+        public static void main(String[] args) {
+            DatagramSocket asocket = null;
+            Scanner ler = new Scanner(System.in);
+            int suaPontuacao = 0, pontuacaoOponente = 0, roundCounter = 0;
+            do {
+                try {
+                    asocket = new DatagramSocket(16868);
+                    byte[] buffer = new byte[1000];
+                    DatagramPacket request =
+                            new DatagramPacket(buffer, buffer.length);
+                    System.out.println("Aguardando jogada do cliente....");
+                    asocket.receive(request);
+                    String strJogadaCliente = new String(buffer, 0, request.getLength());
+
+
+                    //======================================================================
+                    int escolha;
+                    Scanner in = new Scanner(System.in);
+                    System.out.println(ANSI_BLUE + "======JOKENPO=======" + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "O jogador que fizer 3 pontos primeiro ganha." + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "ROUND "+ (roundCounter+1) + "             " + ANSI_RESET);
+                    System.out.print("Voce: ");
+                    if(suaPontuacao != 0){
+                        for (int j = 0; j < suaPontuacao; j++){
+                            System.out.print(ANSI_GREEN + "*" + ANSI_RESET);
+                        }
+                    }
+                    System.out.println();
+                    System.out.print("Oponente: ");
+                    if(pontuacaoOponente != 0){
+                        for (int j = 0; j < pontuacaoOponente; j++){
+                            System.out.print(ANSI_GREEN + "*" + ANSI_RESET);
+                        }
+                    }
+                    System.out.println();
+                    System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "|" + ANSI_RESET + " 1. Pedra         " + ANSI_BLUE + "|" + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "|" + ANSI_RESET + " 2. Papel         " + ANSI_BLUE + "|" + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "|" + ANSI_RESET + " 3. tesoura       " + ANSI_BLUE + "|" + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                    roundCounter++;
+                    String jogada;
+                    do {
+                        System.out.println("Digite a jogada desejada: ");
+                        System.out.println("Somente serão válidos valores entre 1 e 3.");
+                        escolha = in.nextInt();
+                    } while (escolha < 1 || escolha > 3);
+                    switch (escolha) {
+                        case 1:
+                            System.out.println("Voce escolheu " + ANSI_BLUE + "PEDRA" + ANSI_RESET);
+                            jogada = "Pedra";
+                            break;
+                        case 2:
+                            System.out.println("Voce escolheu " + ANSI_BLUE + "PAPEL" + ANSI_RESET);
+                            jogada = "Papel";
+                            break;
+                        case 3:
+                            System.out.println("Voce escolheu " + ANSI_BLUE + "TESOURA" + ANSI_RESET);
+                            jogada = "Tesoura";
+                            break;
+                        default:
+                            jogada = "nada";
+                            break;
+                    }
+                    //==========================================================
+                    byte[] m = jogada.getBytes();
+                    DatagramPacket reply =
+                            new DatagramPacket(m, m.length, request.getAddress(), request.getPort());
+                    System.out.println("Jogada enviada...");
+                    asocket.send(reply);
+
+                    if (jogada.equals(strJogadaCliente)) {
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        System.out.println("  Voce:     " + jogada);
+                        System.out.println("  Oponente: " + strJogadaCliente);
+                        System.out.println(ANSI_YELLOW + "  Houve um empate!");
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                    } else if (jogada.equals("Pedra") && strJogadaCliente.equals("Papel")) {
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        System.out.println("  Voce:     " + jogada);
+                        System.out.println("  Oponente: " + strJogadaCliente);
+                        System.out.println(ANSI_RED + "  Voce perdeu!" + ANSI_RESET);
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        pontuacaoOponente++;
+                    } else if (jogada.equals("Pedra") && strJogadaCliente.equals("Tesoura")) {
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        System.out.println("  Voce:     " + jogada);
+                        System.out.println("  Oponente: " + strJogadaCliente);
+                        System.out.println(ANSI_GREEN + "  Voce ganhou!" + ANSI_RESET);
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        suaPontuacao++;
+                    } else if (jogada.equals("Papel") && strJogadaCliente.equals("Pedra")) {
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        System.out.println("  Voce:     " + jogada);
+                        System.out.println("  Oponente: " + strJogadaCliente);
+                        System.out.println(ANSI_GREEN + "  Voce ganhou!" + ANSI_RESET);
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        suaPontuacao++;
+                    } else if (jogada.equals("Papel") && strJogadaCliente.equals("Tesoura")) {
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        System.out.println("  Voce:     " + jogada);
+                        System.out.println("  Oponente: " + strJogadaCliente);
+                        System.out.println(ANSI_RED + "  Voce perdeu!" + ANSI_RESET);
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        pontuacaoOponente++;
+                    } else if (jogada.equals("Tesoura") && strJogadaCliente.equals("Pedra")) {
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        System.out.println("  Voce:     " + jogada);
+                        System.out.println("  Oponente: " + strJogadaCliente);
+                        System.out.println(ANSI_RED + "  Voce perdeu!" + ANSI_RESET);
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        pontuacaoOponente++;
+                    } else if (jogada.equals("Tesoura") && strJogadaCliente.equals("Papel")) {
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        System.out.println("  Voce:     " + jogada);
+                        System.out.println("  Oponente: " + strJogadaCliente);
+                        System.out.println(ANSI_GREEN + "  Voce ganhou!" + ANSI_RESET);
+                        System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
+                        suaPontuacao++;
+                    }
+                    if(suaPontuacao == 3){
+                        System.out.println(ANSI_GREEN + "  Parabens! Voce conseguiu os 3 pontos antes do seu oponente!" + ANSI_RESET);
+                        break;
+                    } else if(pontuacaoOponente == 3){
+                        System.out.println(ANSI_GREEN + "  Ops... Parece que seu oponente conseguiu os 3 pontos antes de voce." + ANSI_RESET);
+                        break;
+                    }
+
+//System.out.println("Resposta: " + new String(reply.getData(),0,reply.getLength()));
+                } catch (SocketException ex) {
+                    Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (UnknownHostException ex) {
+                    Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+                } finally {
+                    if (asocket != null)
+                        asocket.close();
+                }
+            }while(suaPontuacao != 3 || pontuacaoOponente !=3);
+        }
+
+    }
+
