4986cf8ce06746ff003dd1f4ea1cefbb2ce65a7c
Hildemir <hildemir.regis@hotmail.com>
2020-06-25 22:10:04 -0300

    Added comments

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,14 +1,6 @@
-
-    /*
-     * To change this license header, choose License Headers in Project Properties.
-     * To change this template file, choose Tools | Templates
-     * and open the template in the editor.
-     */
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -17,18 +9,22 @@ import java.util.logging.Logger;
 
 
     public class UDPServer {
-
-public static final String ANSI_RED = "\u001B[31m";
+        // [Constantes das cores]
+        public static final String ANSI_RED = "\u001B[31m";
         public static final String ANSI_GREEN = "\u001B[32m";
         public static final String ANSI_RESET = "\u001B[0m";
         public static final String ANSI_YELLOW = "\u001B[33m";
         public static final String ANSI_BLUE = "\u001B[34m";
+
         public static void main(String[] args) {
+
             DatagramSocket asocket = null;
             Scanner ler = new Scanner(System.in);
             int suaPontuacao = 0, pontuacaoOponente = 0, roundCounter = 0;
+
             do {
                 try {
+                    // [Recebendo jogada do cliente]
                     asocket = new DatagramSocket(16868);
                     byte[] buffer = new byte[1000];
                     DatagramPacket request =
@@ -37,8 +33,7 @@ public static final String ANSI_RED = "\u001B[31m";
                     asocket.receive(request);
                     String strJogadaCliente = new String(buffer, 0, request.getLength());
 
-
-                    //======================================================================
+                    // [Menu]
                     int escolha;
                     Scanner in = new Scanner(System.in);
                     System.out.println(ANSI_BLUE + "======JOKENPO=======" + ANSI_RESET);
@@ -66,6 +61,9 @@ public static final String ANSI_RED = "\u001B[31m";
                     System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
                     roundCounter++;
                     String jogada;
+                    // [Fim do menu]
+
+                    // [Captando jogada do servidor]
                     do {
                         System.out.println("Digite a jogada desejada: ");
                         System.out.println("Somente serão válidos valores entre 1 e 3.");
@@ -88,13 +86,15 @@ public static final String ANSI_RED = "\u001B[31m";
                             jogada = "nada";
                             break;
                     }
-                    //==========================================================
+
+                    // [Enviando jogada para o cliente]
                     byte[] m = jogada.getBytes();
                     DatagramPacket reply =
                             new DatagramPacket(m, m.length, request.getAddress(), request.getPort());
                     System.out.println("Jogada enviada...");
                     asocket.send(reply);
 
+                    // [Exibindo resultado por partida]
                     if (jogada.equals(strJogadaCliente)) {
                         System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
                         System.out.println("  Voce:     " + jogada);
@@ -144,6 +144,8 @@ public static final String ANSI_RED = "\u001B[31m";
                         System.out.println(ANSI_BLUE + "====================" + ANSI_RESET);
                         suaPontuacao++;
                     }
+
+                    // [Exibindo resultado do jogo]
                     if(suaPontuacao == 3){
                         System.out.println(ANSI_GREEN + "  Parabens! Voce conseguiu os 3 pontos antes do seu oponente!" + ANSI_RESET);
                         break;
@@ -151,8 +153,7 @@ public static final String ANSI_RED = "\u001B[31m";
                         System.out.println(ANSI_GREEN + "  Ops... Parece que seu oponente conseguiu os 3 pontos antes de voce." + ANSI_RESET);
                         break;
                     }
-
-//System.out.println("Resposta: " + new String(reply.getData(),0,reply.getLength()));
+                    
                 } catch (SocketException ex) {
                     Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (UnknownHostException ex) {
